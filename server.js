a862384b15353086c29cfc8ebea62d5a9f1e0489
sarthak <nandini.srivastava.12.2004@gmail.com>
2023-06-14 18:03:13 +0530

    second

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const app =express();
 
 //used to parse req.body in express -> PUT or POST
 const bodyparser=require('body-parser');
+const { default: mongoose } = require('mongoose');
 
 //specifically parse JSON data & add it to the request.Body object
 app.use(bodyparser.json());
 
 //Activate the server on 3000 port
-app.listen(3000,()=>{
-console.log("Server Started at port no. 3000")
+app.listen(8000,()=>{
+console.log("Server Started at port no. 9000")
 });
 
 //Routes
@@ -24,3 +25,9 @@ app.post('/api/cars',(request,response)=>{
     response.send("Car Submitted Succesfully.")
 
 })
+mongoose.connect('mongodb://localhost:27017/myDatabase',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(()=>{console.log("Connection Succsessful")})
+.catch((error)=>{console.log("Recieved an error")});
