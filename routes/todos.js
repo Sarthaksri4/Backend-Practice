34e8d7f0b6a311a9a7c2d580b7f673a1fe14fa05
sarthak <nandini.srivastava.12.2004@gmail.com>
2023-06-19 01:58:57 +0530

    updateTodo

--- routes/todos.js
+++ routes/todos.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 //import controller
 const { createTodo } = require("../controllers/createTodo");
-const { getTodos,getTodoById} = require("../controllers/getTodo.js");
-
+const { getTodos,getTodoById } = require("../controllers/getTodo.js");
+const { updateTodo} = require("../controllers/updateTodo");
 
 //define APi routes
 router.post("/createTodo", createTodo);
 router.get("/getTodos", getTodos);
 router.get("/getTodos/:id", getTodoById);
+router.put("/updateTodo/:id", updateTodo);
 
 module.exports = router;
