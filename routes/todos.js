532a2c8b1f6f71f9be1194d0723d1d4eaa79ee93
sarthak <nandini.srivastava.12.2004@gmail.com>
2023-06-19 01:36:00 +0530

    Extract todo items basis on ID

--- routes/todos.js
+++ routes/todos.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 //import controller
 const { createTodo } = require("../controllers/createTodo");
-const { getTodos } = require("../controllers/getTodo.js");
+const { getTodos,getTodoById} = require("../controllers/getTodo.js");
 
 
 //define APi routes
 router.post("/createTodo", createTodo);
 router.get("/getTodos", getTodos);
+router.get("/getTodos/:id", getTodoById);
 
 module.exports = router;
